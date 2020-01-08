869bf228e6883267f437c475c4bc9f9f1caffb5d
Helegbedey <devaze007@gmail.com>
2020-01-08 21:26:11 +0100

    setting up view engine for express app

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+//Required Modules
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const SignupController = require('./controllers/signup');
 
-const app = express()
+//Initializations
+const app = express();
 
-const PORT = 3000;
+//Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser({extended:false}));
+
+//set view engine
+app.set('views',path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({
+    defaultLayout:"main",
+}));
+app.set('view engine', 'handlebars')
+//set static folder
+app.use('/public/', express.static(path.join(__dirname,'public')));
+
+//Controllers
+app.get('/signup', SignupController);
+
+
+const PORT = 5000;
 
 app.listen(PORT,()=>{
     console.log(`app now listening to port ${PORT}`)
-})
+});
