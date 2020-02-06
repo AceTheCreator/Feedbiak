2380a7828eac1eefbe186f1dee0169082178ffd9
Helegbedey <devaze007@gmail.com>
2020-02-06 17:43:24 +0100

    working on my .env variables

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.use(posts);
 // Email ivitation routes
 // app.use(invite);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`app now listening to port ${PORT}`);
